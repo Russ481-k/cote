09cab66b226d2b0f15d513c3d1469da8a26da09e
Russ <yunsubin481@gmail.com>
2023-03-07 20:02:39 +0900

    11

---
+++ test1 copy 11.js
@@ -0,0 +1,20 @@
+/*
+
+    https://www.acmicpc.net/problem/18108
+2541
+*/
+
+let fs = require('fs');
+// let input = fs.readFileSync('/dev/stdin').toString().trim();
+let input = fs.readFileSync('./input11.txt').toString();
+input = input.split(' ');
+
+    const Solution = (input) => {
+        console.log(Number(input)-543)
+
+
+    }
+    Solution(input[0])
+
+//1~A 까지의 수
+//B개의 세트
