66193f91477227237fa139511bd6f5aa216bafba
Russ <yunsubin481@gmail.com>
2023-03-08 22:04:01 +0900

    15

---
+++ test1 copy 15.js
@@ -0,0 +1,23 @@
+
+
+/*
+    https://www.acmicpc.net/problem/10171
+\    /\
+ )  ( ')
+(  /  )
+ \(__)|
+*/
+
+let fs = require('fs');
+// let input = fs.readFileSync('/dev/stdin').toString();
+let input = fs.readFileSync('./input14.txt').toString();
+input = input.split(' ');
+
+    const Solution = () => {
+
+        console.log(`\\    /\\`)
+        console.log(` )  ( ')`)
+        console.log(`(  /  )`)
+        console.log(` \\(__)|`);
+    }
+    Solution()
