1b89cedf8e66b14202738020f7c95048f4f9fb49
Russ <yunsubin481@gmail.com>
2023-03-08 22:03:57 +0900

    16

---
+++ test1 copy 16.js
@@ -0,0 +1,24 @@
+
+
+/*
+    https://www.acmicpc.net/problem/10172
+|\_/|
+|q p|   /}
+( 0 )"""\
+|"^"`    |
+||_/=\\__|
+*/
+
+let fs = require('fs');
+// let input = fs.readFileSync('/dev/stdin').toString();
+let input = fs.readFileSync('./input14.txt').toString();
+
+    const Solution = () => {
+
+        console.log(`|\\_/|`)
+        console.log(`|q p|   /}`)
+        console.log(`( 0 )"""\\`)
+        console.log(`|"^"\`    |`)
+        console.log(`||_/=\\\\__|`);
+    }
+    Solution()
