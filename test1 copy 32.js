b93978fae99d5b5c07c128dac5ca25563326adee
Russ <yunsubin481@gmail.com>
2023-03-14 10:34:17 +0900

    32

--- test1 copy 32.js
+++ test1 copy 32.js
@@ -7,18 +7,18 @@
 3 4
 9 8
 5 2
+2 3
+3 4
+9 8
+5 2
 0 0
 */
 
-var fs = require('fs');
-const input = require('fs').readFileSync('./input32.txt').toString().split('\n');
-// const input = require('fs').readFileSync('/dev/stdin').toString().split('\n');
-let answer = ""
-let cnt = input.length-1
-while(cnt>0){
-    let b = parseInt(input[i].split(' ')[0]);
-    let c = parseInt(input[i].split(' ')[1]);
-    answer+=(b+c).toString()+"\n"
-    cnt--
+
+const input = require('fs').readFileSync('./input32.txt').toString().trim().split('\n');
+
+for (let i = 0; i < input.length - 1; i++) {
+    let numbers = input[i].split(' ').map(v => v * 1);
+    
+    console.log(numbers[0] + numbers[1]);
 }
-console.log(answer)   
